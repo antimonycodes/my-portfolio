f64f7e275d70c4c98aa7d79e1e78c0a9745803db
antimonycodes <akinrinadetobilobasb@gmail.com>
2024-03-26 11:59:05 +0100

    first commit

---
+++ components/ui/Nav.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import Link from "next/link"
+
+export default function Nav() {
+    const links =[
+        {
+            path: "/",
+            name: "home"
+        },
+        {
+            path: "/projects",
+            name: "projects"
+        },
+        {
+            path: "/about",
+            name: "about"
+        },
+        {
+            path: "/contact",
+            name: "contact"
+        }
+    ]
+    return(
+        <nav>
+            <ul>
+                {links.map(link=> {
+
+                    return(
+                        <li key={link.path}>
+                            <Link href={link.path}>
+                                <span>{link.name}</span>
+                            </Link>
+                        </li>
+                    )
+
+                })}
+               
+            </ul>
+        </nav>
+     
+    )
+    
+}
