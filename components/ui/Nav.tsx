70118205b1f6fedf4a0d538ae7c6ccdb15f587ae
antimonycodes <akinrinadetobilobasb@gmail.com>
2024-03-27 16:49:46 +0100

    update

--- components/ui/Nav.tsx
+++ components/ui/Nav.tsx
@@ -1,44 +1,106 @@
-'use client'
+"use client";
 
-import Link from "next/link"
-
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import {
+  AnimatePresence,
+  MotionValue,
+  motion,
+  useMotionValue,
+} from "framer-motion";
 export default function Nav() {
-    const links =[
-        {
-            path: "/",
-            name: "home"
-        },
-        {
-            path: "/projects",
-            name: "projects"
-        },
-        {
-            path: "/about",
-            name: "aboutss"
-        },
-        {
-            path: "/contact",
-            name: "contacts"
-        }
-    ]
-    return(
-        <nav>
-            <ul>
-                {links.map(link=> {
+  const links = [
+    {
+      path: "/",
+      name: "home",
+    },
+    {
+      path: "/projects",
+      name: "projects",
+    },
+    {
+      path: "/about",
+      name: "aboutss",
+    },
+    {
+      path: "/contact",
+      name: "contacts",
+    },
+  ];
 
-                    return(
-                        <li key={link.path}>
-                            <Link href={link.path}>
-                                <span>{link.name}</span>
-                            </Link>
-                        </li>
-                    )
+  const pathName = usePathname;
+  const MotionLink = motion(Link);
 
-                })}
-               
-            </ul>
-        </nav>
-     
-    )
-    
+  const mapRange = (
+    inputLower: number,
+    inputUpper: number,
+    outputLower: number,
+    outputUpper: number
+  ) => {
+    const INPUT_RANGE = inputUpper - inputLower;
+    const OUTPUT_RANGE = outputUpper - outputLower;
+
+    return (value: number) =>
+      outputLower + (((value - inputLower) / INPUT_RANGE) * OUTPUT_RANGE || 0);
+  };
+
+  const setTransform = (
+    item: HTMLElement & EventTarget,
+    event: React.PointerEvent,
+    x: MotionValue,
+    y: MotionValue
+  ) => {
+    const bounds = item.getBoundingClientRect();
+    const relativeX = event.clientX - bounds.left;
+    const relativeY = event.clientY - bounds.top;
+    const xRange = mapRange(0, bounds.width, -1, 1)(relativeX);
+    const yRange = mapRange(0, bounds.height, -1, 1)(relativeY);
+    x.set(xRange * 10);
+    y.set(yRange * 10);
+    console.log(xRange);
+  };
+
+  return (
+    <nav className=" p-8">
+      <ul className=" flex gap-12">
+        <AnimatePresence>
+          {links.map((link) => {
+            const x = useMotionValue(0);
+            const y = useMotionValue(0);
+
+            return (
+              <motion.li
+                onPointerMove={(event) => {
+                  const item = event.currentTarget;
+                  setTransform(item, event, x, y);
+                }}
+                key={link.path}
+                onPointerLeave={(event) => {
+                  x.set(0);
+                  y.set(0);
+                }}
+                style={{ x, y }}
+              >
+                <MotionLink
+                  className={cn(
+                    " font-medium rounded-md text-sm py-2 px-4 transition-all duration-500 ease-out hover:bg-slate-200",
+                    pathName === link.path ? "bg-slate-300" : ""
+                  )}
+                  href={link.path}
+                >
+                  <motion.span>{link.name}</motion.span>
+                  {pathName === link.path ? (
+                    <motion.div>
+                      transition{{ type: "spring" }}
+                      layoutId="underline" className=""
+                    </motion.div>
+                  ) : null}
+                </MotionLink>
+              </motion.li>
+            );
+          })}
+        </AnimatePresence>
+      </ul>
+    </nav>
+  );
 }
---
+++ components/ui/Threejs.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React, { useRef, useEffect } from "react";
+import * as THREE from "three";
+
+export default function Threejs() {
+  const mount = useRef<HTMLDivElement>(null); // Specify the type of ref
+
+  useEffect(() => {
+    if (!mount.current) return; // Check if mount.current is null before using it
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    const mountNode = mount.current; // Copy mount.current to a variable
+    mountNode.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    const animate = function () {
+      requestAnimationFrame(animate);
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+    };
+
+    animate();
+
+    return () => {
+      if (mountNode) {
+        // Use the variable inside the cleanup function
+        mountNode.removeChild(renderer.domElement);
+      }
+    };
+  }, []);
+
+  return <div ref={mount} />;
+}
